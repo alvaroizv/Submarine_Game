43e343a90ad05f38bd32b55fd27b5ed22f78a8db
alvaroroobles <alvaro.becerril.robles@gmail.com>
2025-11-11 19:38:00 +0100

    Agregamos tablero a la interfaz gráfica

--- js/UI.js
+++ js/UI.js
@@ -1,23 +1,36 @@
 export const UI = {
-    game : null,
-    control : {
-        board: null,
-        status: null
-    },
-    init: (domControl, game)=>{
-        UI.control.board = document.getElementById(domControl.board);
-        UI.control.status = document.getElementById(domControl.status);
-    },
-    setEvent: (domControl) => {
-        document.getElementById(domControl.btnShot[0]).addEventListener('click', ()=>{
-            domControl.btnShot[1]();
-        });
-    },
-    start(game) {
-        UI.game = game;
-        UI.control.status.textContent = "Juego iniciado";
-    },
-    changeStatus(newStatus) {
-        UI.control.status.textContent = newStatus;
+  game: null,
+  control: {
+    template: null,
+    board: null,
+    status: null,
+  },
+  init: (domControl, game) => {
+    UI.control.board = document.getElementById(domControl.board);
+    UI.control.status = document.getElementById(domControl.status);
+    UI.control.template = document.getElementById(domControl.template);
+  },
+  setEvent: (domControl) => {
+    document
+      .getElementById(domControl.btnShot[0])
+      .addEventListener("click", () => {
+        domControl.btnShot[1]();
+      });
+  },
+  start(game) {
+    UI.game = game;
+    UI.control.status.textContent = "Juego iniciado";
+  },
+  changeStatus(newStatus) {
+    UI.control.status.textContent = newStatus;
+  },
+  generateBoard(columns_number) {
+    console.log(UI.board);
+    UI.control.board.style.gridTemplateColumns = `repeat(${columns_number}, 1fr)`;
+    for (let index = 0; index < columns_number * columns_number; index++) {
+      const clonated_template = UI.control.template.content.cloneNode(true);
+      const box = clonated_template.querySelector(".box");
+      UI.control.board.appendChild(box);
     }
-}
+  },
+};
--- js/game.js
+++ js/game.js
@@ -2,20 +2,21 @@ import { UI } from "./UI.js";
 import { GameSubmarine } from "./GameSubmarine.js";
 
 UI.init({
-    board: 'gameBoard',
-    status: 'gameStatus',
+  board: "gameBoard",
+  status: "gameStatus",
+  template: "box-template",
 });
 
 const gameInstance = new GameSubmarine(UI);
 
 UI.setEvent({
-    btnShot: ['btnShot', ()=> gameInstance.shot()]
+  btnShot: ["btnShot", () => gameInstance.shot()],
 });
 
+UI.generateBoard(8);
 /* Esto es otra forma, cuidado con la perdida del contexto this.
 /*
 UI.setEvent({
     btnShot: ['btnShot', gameInstance.shot.bind(gameInstance)]
 });
 */
-
