6e2fdda0e9b3dc4013c470330e677e6bc4cce555
alvaroizv <abecrob2410@ieszaidinvergeles.org>
2025-12-11 11:14:18 +0100

    Logica de creación de input

--- FormAPI/FormAPI.js
+++ FormAPI/FormAPI.js
@@ -8,20 +8,36 @@ export class FormAPI {
   }
 
   addInput(name, type, placeholder, required) {
-    let regularExpression = null;
+    //let regularExpression = null;
     this.controlForm.push({
       name: name,
       type: type,
       placeholder: placeholder,
-      requiree: required,
-      regularExpression: regularExpression,
+      required: required,
+      //regularExpression: regularExpression,
     });
     this.uiControl.createElementInput(name, type, placeholder, required);
   }
 
   buildForm() {
-    // this.controlForm.forEach(inputData => {
+    this.controlForm.forEach((item) => {});
   }
+
+  getRegularExpression(regexType) {
+    let found = false;
+    const regexList = {
+      dni: "^[0-9]{8}[A-Z]$",
+      email: "^.+@.+..+$",
+    };
+
+    Object.values(regexList).forEach((item) => {
+      if (item === regexType) return item;
+    });
+
+    return null;
+  }
+
+  addError(name, message, regex) {}
 
   clearFormInput(name) {
     this.uiControl.clearFormInput(name);
--- FormAPI/index.js
+++ FormAPI/index.js
@@ -4,3 +4,13 @@ const formAPI = FormAPIBuilder.getFormAPI("formulario");
 
 formAPI.addInput("hola", "text", "este es mi Formulario perro", false);
 console.log("Formulario hecho");
+
+formAPI.addError(
+  "hola",
+  "Este es mi mensaje de error",
+  formAPI.getRegularExpression("email")
+);
+// buildForm();
+// addRegularExpresion();
+// getExpression()
+// y tmb el build
